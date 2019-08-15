7963d7edc9142fd59c1e7b1657237e021d168b74
shelly <shellynacole@yahoo.com>
2019-08-15 18:36:37 -0400

    updated code

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
+const startGame = () => {
+  let play = prompt('Do you want to play?');
+  let name;
+  if (play === 'yes') {
+    name = prompt('Enter your username.')
+    startCombat(play, name);
+  } else if (play === 'no') {
+    console.log('GOODBYE!')
+  }
+};
 
-const startFight = () => {
-    let play = prompt("Do you want to start?");
-    let name;
-    if (play === "yes") {
-      name = prompt("Enter your name")
-      startBattle(play, name);
-    } else if (play === "no") {
-      console.log("GOODBYE.")
-    }
-  };
-  
- 
-  const reduceHealth = () => {
-    return Math.floor(Math.random() * 5) + 1;
-  };
-  
-  const startBattle = (play, name) => {
-    let grantHealth = 10;
-    let playerHealth = 40;
-    let userWins = 0;
-  
-    while (play === "yes") {
+const getDamage = () => {
+  return Math.floor(Math.random() * 5) + 1;
+};
+
+const startCombat = (play, name) => {
+  let grantHealth = 10;
+  let playerHealth = 40;
+  let userWins = 0;
+
+  while (play === 'yes') {
+    const action = prompt('Would you like to "attack" or "quit"?')
+    if (action === 'attack') {
       if (grantHealth <= 0) {
         userWins++;
         grantHealth = 10;
@@ -28,19 +28,23 @@ const startFight = () => {
           console.log(`${name} WINS!`);
           break;
         } else {
-          console.log(`${name} has beaten Grant. ${name} must win ${3 - userWins} more time(s).`);
+          console.log(`${name} has beaten Grant! ${name} must win ${3 - userWins} more time(s).`);
         }
       }
       if (playerHealth <= 0) {
-        console.log("GRANT WINS.");
+        console.log('GRANT WINS.');
         break;
       }
-  
-      playerHealth -= reduceHealth();
-      grantHealth -= reduceHealth();
-      console.log(`${name} was attacked. ${name} has ${playerHealth} health points left.`);
-      console.log(`Grant was attacked. Grant has ${grantHealth} health points left.`);
+
+      playerHealth -= getDamage();
+      grantHealth -= getDamage();
+      console.log(`${name} has ${playerHealth} health point(s) left.`);
+      console.log(`Grant the Might Chicken has ${grantHealth} health point(s)left.`);
+    } else if (action === 'quit') {
+      console.log('GAME OVER!');
+      break;
     }
-  };
-  
-  startFight();
+  }
+};
+
+startGame();
